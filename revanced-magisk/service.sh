346bc4f907296b6c9199d4b77e4a1568b81c926b
Harsh Preet <harshpreets63@gmail.com>
2025-07-23 15:37:05 -0400

    Fix Magisk Module

--- revanced-magisk/service.sh
+++ revanced-magisk/service.sh
@@ -2,15 +2,6 @@
 MODDIR=${0%/*}
 RVPATH=/data/adb/rvhc/${MODDIR##*/}.apk
 . "$MODDIR/config"
-. "$MODDIR/common.sh"
-
-# Define the path of root manager applet bin directories using find and set it to $PATH then export it
-if ! command -v busybox >/dev/null 2>&1; then
-	TOYS_PATH=$(find "/data/adb" -maxdepth 3 \( -name busybox -o -name ksu_susfs \) -exec dirname {} \; | sort -u | tr '\n' ':')
-	export PATH="${PATH:+${PATH}:}${TOYS_PATH%:}"
-fi
-
-HAS_SUSFS="$(command -v ksu_susfs)"
 
 err() {
 	[ ! -f "$MODDIR/err" ] && cp "$MODDIR/module.prop" "$MODDIR/err"
@@ -20,7 +11,7 @@ err() {
 until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 until [ -d "/sdcard/Android" ]; do sleep 1; done
 while
-	BASEPATH=$(pmex path "$PKG_NAME")
+	BASEPATH=$(pm path "$PKG_NAME" 2>&1 </dev/null)
 	SVCL=$?
 	[ $SVCL = 20 ]
 do sleep 2; done
@@ -42,18 +33,15 @@ run() {
 		err "version mismatch (installed:${VERSION}, module:$PKG_VER)"
 		return
 	fi
-	mz grep "$PKG_NAME" /proc/mounts | while read -r line; do
+	grep "$PKG_NAME" /proc/mounts | while read -r line; do
 		mp=${line#* } mp=${mp%% *}
-		mz umount -l "${mp%%\\*}"
+		umount -l "${mp%%\\*}"
 	done
 	if ! chcon u:object_r:apk_data_file:s0 "$RVPATH"; then
 		err "apk not found"
 		return
 	fi
-	$HAS_SUSFS && ksu_susfs add_sus_kstat "$BASEPATH"
-	[ -n "$BASEPATH" ] && mz mount "$RVPATH" "$BASEPATH/base.apk"
-	$HAS_SUSFS && ksu_susfs update_sus_kstat "$BASEPATH"
-	$HAS_SUSFS && ksu_susfs add_sus_mount "$BASEPATH"
+	mount -o bind "$RVPATH" "$BASEPATH/base.apk"
 	am force-stop "$PKG_NAME"
 	[ -f "$MODDIR/err" ] && mv -f "$MODDIR/err" "$MODDIR/module.prop"
 }
